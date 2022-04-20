28d194a5ae9bf3bd8a9b3a68af9f884396f0f6f2
Szymon Świędrych <szymek_sw@outlook.com>
2022-04-20 17:00:21 +0200

    Update TUI

--- source/main.py
+++ source/main.py
@@ -1,3 +1,8 @@
+import logging
+from enum import Enum
+
+import serial
+
 import check_sum as cs
 import serial as ser
 
@@ -6,62 +11,111 @@ import xmodem
 
 def main():
     user_input = ""
+    serial_port_name = "None"
+    baudrate = "None"
+    check_sum = "Checksum"
+    debugging = False
+
     while user_input != "q":
+        print(f"Port: {serial_port_name}")
+        print(f"Baudrate: {baudrate}")
+        print(f"Typ sumy kontrolnej: {check_sum}")
+        print(f"Tryb debugowania: {debugging}")
+        print()
+
         user_input = input('''Podaj, czy chcesz:
         (1) Wysłać pakiet
         (2) Odebrać pakiet
+        (3) Zmien ustawienia portu szeregowego
+        (4) Zmien typ sumy kontrolnej
+        (5) Przełącz tryb debugowania
         [q - wyjście]\n> ''')
         match user_input:
             case '1':
-                send_packet()
+                send_packet(serial_port_name, baudrate)
             case '2':
-                receive_packet()
+                receive_packet(serial_port_name, baudrate, check_sum)
+            case '3':
+                serial_port_name, baudrate = get_port_io()
+            case '4':
+                if check_sum == "CRC":
+                    check_sum = "Checksum"
+                else:
+                    check_sum = "CRC"
+            case '5':
+                debugging = not debugging
+                if debugging:
+                    logging.basicConfig(level=logging.DEBUG)
+                else:
+                    logging.basicConfig(level=logging.ERROR)
             case _:
                 continue
 
 
-def send_packet():
-    message = ""
+def send_packet(port_name, baudrate):
+    data = bytes()
     # read user input
     match input("Wiadomość:\n\t(1) Predefiniowana\n\t(2) Wpisana ręcznie\n\t(3) Z pliku\n> "):
         case '1':
-            message = Examples.invocation
+            data = bytes(Examples.invocation.value, 'utf-8')
         case '2':
-            message = input("Podaj wiadomość: ")
+            data = input("Podaj wiadomość: ")
+            data = bytes(data, 'utf-8')
         case '3':
-            message = message_from_file(input("Podaj ścieżkę: "))
+            data = message_from_file(input("Podaj ścieżkę: "))
 
-    # initialize port and send message
-    port_name = "/dev/pts/3"
-    baudrate = 9600
-    timeout = 3
+    try:
+        serial_port = xmodem.initialize_serial(port_name, baudrate)
+        serial_port.open()
+        xmodem.send(serial_port, data)
+        serial_port.close()
+    except (serial.SerialException, ValueError):
+        print("Ustawienia Portu są nie poprawne, spróbuj ponownie")
+    except xmodem.ReceiverSendUnexpectedResponseException:
+        print("Odbiorca nie rozpoczął transmisji, spróbuj ponownie")
 
+
+def receive_packet(port_name, baudrate, check_sum):
     serial_port = xmodem.initialize_serial(port_name, baudrate)
-    xmodem.send(serial_port, bytes(message, 'ascii'))
 
-    serial_port.close()
+    try:
+        serial_port.open()
+        if check_sum == "CRC":
+            check_sum = xmodem.CheckSumEnum.crc
+        else:
+            check_sum = xmodem.CheckSumEnum.algebraic
+        data = xmodem.receive(serial_port, check_sum)
+        serial_port.close()
+    except (serial.SerialException, ValueError):
+        print("Ustawienia Portu są nie poprawne, spróbuj ponownie")
+    except xmodem.SenderDoesNotAcceptTransferException:
+        print("Nadawca nie zaakceptował transmisji, spróbuj ponownie")
 
-
-def receive_packet():
-    port_name = "/dev/pts/4"
-    baudrate = 9600
-
-    serial_port = xmodem.initialize_serial(port_name, baudrate)
-    data = xmodem.receive(serial_port, xmodem.CheckSumEnum.crc)
     print("Cała wiadomość: ")
     print(data.decode("utf-8"))
-    serial_port.close()
+
+    path = input("Podaj gdzie zapisać: ")
+    message_to_file(data, path)
+
+
+def get_port_io():
+    port_name = input("Podaj nazwę portu: ")
+    baudrate = int(input("Podaj baudrate: "))
+    return port_name, baudrate
 
 
 def message_from_file(path):
-    with open(path, "r") as message_file:
-        result = ""
-        for line in message_file:
-            result += line
+    with open(path, "rb") as message_file:
+        result = message_file.read()
     return result
 
 
-class Examples:
+def message_to_file(message: bytes, path):
+    with open(path, "wb+") as message_file:
+        message_file.write(message)
+
+
+class Examples(Enum):
     invocation = "Litwo! Ojczyzno moja! Ty jestes jak zdrowie," \
                  "Ile cie trzeba cenic, ten tylko sie dowie," \
                  "Kto cie stracil. Dzis pieknosc twa w calej ozdobie " \
--- source/xmodem.py
+++ source/xmodem.py
@@ -34,7 +34,6 @@ def initialize_serial(port: str, baudrate: int = 9600, timeout=3):
     serial_port.parity = ser.PARITY_NONE
     serial_port.stopbits = ser.STOPBITS_ONE
     serial_port.bytesize = ser.EIGHTBITS
-    serial_port.open()
     return serial_port
 
 
